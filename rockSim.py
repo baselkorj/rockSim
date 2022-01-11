d93e683d527785ff6ae103b170c34b77b4f951c3
Mashal Al-Busaidi <Mashal1996@gmail.com>
2022-01-11 12:23:53 +0400

    Update the decode function and netCheck function

--- rockSim.py
+++ rockSim.py
@@ -1,6 +1,10 @@
-import serial, requests, json, time, random
+import serial
+import requests
+import json
+import time
+import random
 
-ser = serial.Serial('COM2')
+ser = serial.Serial("COM7", baudrate=19200)
 
 ser.flushInput()
 
@@ -10,71 +14,70 @@ packet = ""
 api = "https://whiskershub.aphcarios.com/api/whiskers/Device/test/"
 postBuffer = ""
 packet = ""
-
+netAv = True
 print("Serial Port Initialized.")
 
 print("Listening to Gateway...")
 
+
 def decode():
     global bytes, postBuffer
     ser_decode = ser_byte.decode("utf-8")
+
     try:
-        if ser_decode == "\r" or ser_decode == "\n":
+        bytes = bytes + ser_decode
+        if ser_decode == "\r":
+            ser.write((bytes + "\n").encode())
             if bytes != "":
                 print("\nReceived: ", end=bytes)
-            
+
             if bytes == "":
                 return
-            
+
             elif bytes == "AT":
-                time.sleep(random.randint(0, 100)/1000)
+                time.sleep(random.randint(0, 100) / 1000)
                 print("\nResponse: OK")
-                ser.write("OK\r".encode())
-            
+
+                ser.write("\nOK\r\n".encode())
             elif bytes[0:5] == "AT&K0":
-                time.sleep(random.randint(0, 100)/1000)
+                time.sleep(random.randint(0, 100) / 1000)
                 print("\nResponse: OK")
-                ser.write("OK\r".encode())
-            
+                ser.write("\nOK\r\n".encode())
             elif bytes[0:9] == "AT+SBDWT=":
-                time.sleep(random.randint(0, 100)/1000)
+                time.sleep(random.randint(0, 100) / 1000)
                 postBuffer = bytes[9:]
                 print("\nPOST Buffer: ", end=postBuffer)
-                ser.write("OK\r".encode())
-            
+                ser.write("\nOK\r\n".encode())
             elif bytes[0:8] == "AT+SBDIX":
                 if netCheck():
-                    time.sleep(random.randint(0, 5000)/1000)
+                    time.sleep(random.randint(0, 5000) / 1000)
                     print("\nResponse: +SBDIX: 0, 0, 0, 0, 0, 0")
                     ser.write("+SBDIX: 0, 0, 0, 0, 0, 0\r".encode())
                     packet = '[{"data":"' + postBuffer + '"}]'
-                    post(api+decoderTag, json.loads(packet))
+                    # post(api+decoderTag, json.loads(packet))
                 else:
                     print("\nResponse: +SBDIX: 32, 0, 0, 0, 0, 0")
                     ser.write("+SBDIX: 32, 0, 0, 0, 0, 0\r".encode())
-            
+
             else:
                 print("\nUnknown AT Command.")
-            
             bytes = ""
-        else:
-            if ser_decode != "\r" and ser_decode != "\n":
-                bytes = bytes + ser_decode
+
     except:
         bytes = ""
 
+
 def post(url, json):
-    x = requests.post(url+decoderTag, json=json)
+    x = requests.post(url + decoderTag, json=json)
     print("\nPOST Response: ", end=str(x))
     print("\nPOST Response Body:")
     print(x.text)
 
+
 def netCheck():
-    try:
-        request = requests.get("https://www.google.com", timeout=2)
-        return True
-    except:
-        return False
+    global netAv
+    return netAv
+
 
 while True:
     ser_byte = ser.read()
